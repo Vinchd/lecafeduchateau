1b9810733b46753fd8ac38c9bf72bc6f5449bd32
Vinchd <vince.daviaud@gmail.com>
2025-08-01 16:41:40 +0200

    image on not-found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,8 +1,17 @@
+"use client";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
 	return (
-		<div className="relative flex flex-col justify-center items-center gap-10 bg-primary h-[100dvh] min-h-screen text-2xl">
+		<div className="relative flex flex-col justify-center items-center gap-10 h-[100dvh] min-h-screen text-2xl">
+			<Image
+				src="/lecafeduchateau-logo.jpg"
+				alt="Plat Ceviche Thon"
+				sizes="(max-width: 500px) 100vw, (max-width: 800px) 100vw, (max-width: 1080px) 100vw, 100vw"
+				width={300}
+				height={424}
+			/>
 			<div>Page non trouvée</div>
 			<Link
 				href="/"
