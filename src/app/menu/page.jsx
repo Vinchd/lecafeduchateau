3f303c6bffe9801e927e089e1b74a66bae3e5a9f
Vinchd <vince.daviaud@gmail.com>
2025-08-21 20:21:31 +0200

    add menu du dimanche in menu page & rework getMenu function

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,64 +1,110 @@
 import Papa from "papaparse";
 
 async function getMenu() {
-	const res = await fetch(
-		process.env.GOOGLE_SHEET_URL,
-		// { next: { revalidate: 10 } }, // ⏱️ revalidation toutes les heures
-		{ cache: "no-store" },
-	);
+	const res = await fetch(process.env.GOOGLE_SHEET_URL, {
+		next: { revalidate: 10 },
+	});
 	const csv = await res.text();
 	const parsed = Papa.parse(csv, { header: true }).data;
 
-	const menu = parsed.reduce(
+	const { menuPrincipal, menuDimanche } = parsed.reduce(
 		(acc, { Id, Catégorie, Nom, Description, Prix }) => {
 			if (!Catégorie || !Nom) return acc;
-			if (!acc[Catégorie]) acc[Catégorie] = [];
-			acc[Catégorie].push({
+
+			// Préparer l'objet de base
+			const item = {
 				id: Id,
 				nom: Nom,
 				description: Description,
 				prix: Prix?.replace(/,00$/, ""),
-			});
+			};
+
+			// Menu classique
+			if (Catégorie === "Food" || Catégorie === "Dessert") {
+				if (!acc.menuPrincipal[Catégorie]) acc.menuPrincipal[Catégorie] = [];
+				acc.menuPrincipal[Catégorie].push(item);
+			}
+
+			// Menu du dimanche
+			if (
+				Catégorie === "Dimanche - Food" ||
+				Catégorie === "Dimanche - Dessert"
+			) {
+				if (!acc.menuDimanche[Catégorie]) acc.menuDimanche[Catégorie] = [];
+				acc.menuDimanche[Catégorie].push(item);
+			}
+
 			return acc;
 		},
-		{},
+		{ menuPrincipal: {}, menuDimanche: {} },
 	);
 
-	return menu;
+	return { menuPrincipal, menuDimanche };
 }
 
 export default async function page() {
-	const menu = await getMenu();
+	const { menuPrincipal, menuDimanche } = await getMenu();
 
 	return (
 		<main className="relative flex flex-col bg-primary pt-12 pb-12 h-full text-secondary">
 			<section className="flex-grow mx-auto max-w-3xl overflow-y-auto scrollbar-hide">
-				<h1 className="mb-10 font-sharpie text-5xl text-center">Menu</h1>
-				{Object.entries(menu).map(([category, items], idx, arr) => (
-					<div key={category} className="mb-6 font-gambetta uppercase">
-						<ul className="flex flex-col items-center gap-6">
-							{items.map((item) => (
-								<li
-									key={item.id}
-									className="flex flex-col items-center w-full leading-[1.15]"
-								>
-									<p className="font-semibold text-[17px] max-sm:text-[clamp(12px,3vw,16px)] text-center">
-										{item.nom} – {item.prix} €
-									</p>
-									{item.description && (
-										<p className="w-1/2 max-sm:text-[clamp(11px,3vw,15px)] text-center">
-											{item.description}
+				<div className="mb-24">
+					<h1 className="mb-10 font-sharpie text-5xl text-center">Menu</h1>
+					{Object.entries(menuPrincipal).map(([category, items], idx, arr) => (
+						<div key={category} className="mb-6 font-gambetta uppercase">
+							<ul className="flex flex-col items-center gap-6">
+								{items.map((item) => (
+									<li
+										key={item.id}
+										className="flex flex-col items-center w-full leading-[1.15]"
+									>
+										<p className="font-semibold text-[17px] max-sm:text-[clamp(12px,3vw,16px)] text-center">
+											{item.nom} – {item.prix} €
 										</p>
-									)}
-								</li>
-							))}
+										{item.description && (
+											<p className="w-1/2 max-sm:text-[clamp(11px,3vw,15px)] text-center">
+												{item.description}
+											</p>
+										)}
+									</li>
+								))}
 
-							{idx < arr.length - 1 && (
-								<hr className="my-8 border-secondary border-t w-30" />
-							)}
-						</ul>
-					</div>
-				))}
+								{idx < arr.length - 1 && (
+									<hr className="my-8 border-secondary border-t w-30" />
+								)}
+							</ul>
+						</div>
+					))}
+				</div>
+				<div>
+					<h2 className="mb-10 font-sharpie text-5xl text-center">
+						Les dimanches du Château
+					</h2>
+					{Object.entries(menuDimanche).map(([category, items], idx, arr) => (
+						<div key={category} className="mb-6 font-gambetta uppercase">
+							<ul className="flex flex-col items-center gap-6">
+								{items.map((item) => (
+									<li
+										key={item.id}
+										className="flex flex-col items-center w-full leading-[1.15]"
+									>
+										<p className="font-semibold text-[17px] max-sm:text-[clamp(12px,3vw,16px)] text-center">
+											{item.nom} – {item.prix} €
+										</p>
+										{item.description && (
+											<p className="w-1/2 max-sm:text-[clamp(11px,3vw,15px)] text-center">
+												{item.description}
+											</p>
+										)}
+									</li>
+								))}
+								{idx < arr.length - 1 && (
+									<hr className="my-8 border-secondary border-t w-30" />
+								)}
+							</ul>
+						</div>
+					))}
+				</div>
 			</section>
 		</main>
 	);
